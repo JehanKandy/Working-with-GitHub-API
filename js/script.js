bb8731fc727b0ebf1791b608b910d4f4558d4b3f
JehanKandy <jehankandy@gmail.com>
2023-01-31 10:04:07 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@ var gitApi = document.getElementById('git_api');
 
 gitApi.addEventListener('submit',function(git){
     var searchValue = document.getElementById('search').value
-    alert(searchValue);
+    var newName = searchValue.split(' ').join('');
+
+    fetch("https://api.github.com/users/"+newName)
+    //result convert to json
+
+    .then((result) => result.json())
+    .then((data) => {
+        console.log(data)
+    })
+
 })
